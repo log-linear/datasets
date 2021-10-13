0536085eb873f8396333cfb5c596f37b9d8b9e18
victor.faner <victor.faner@dallascityhall.com>
2021-10-13 16:45:11 -0500

    Remove unneeded cd

--- scripts/run_scripts.ps1
+++ scripts/run_scripts.ps1
@@ -1,6 +1,4 @@
 # Script to run updates and auto commit/push to GitHub 
-cd C:\Users\victor.faner\Documents\datasets\scripts
-
 rscript .\dfw_historical_weather.R
 git add ..\data\dfw_historical_weather.csv
 $today = Get-Date -Format "yyyy/MM/dd"
