c8734f129570e2d7afaaadd8e615717cd600f011
victor.faner <victor.faner@dallascityhall.com>
2021-10-18 09:32:08 -0500

    Change auto-commit date format

--- scripts/run_scripts.ps1
+++ scripts/run_scripts.ps1
@@ -1,7 +1,7 @@
 # Script to run updates and auto commit/push to GitHub 
 rscript .\dfw_historical_weather.R
 git add ..\data\dfw_historical_weather.csv
-$today = Get-Date -Format "yyyy/MM/dd"
+$today = Get-Date -Format "yyyy-MM-dd"
 $message = -join("Update ", $today)
 git commit -m $message
 git push origin
